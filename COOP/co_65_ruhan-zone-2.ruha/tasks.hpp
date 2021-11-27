ede4c03ebcaa8455a30b4c9122318ee479c3e689
3Mydlo3 <mydlo333@interia.pl>
2021-11-27 18:45:55 +0100

    Replace CSTRING with LSTRING in tasks
    This way title/description will be localized clientside

--- COOP/co_65_ruhan-zone-2.ruha/tasks.hpp
+++ COOP/co_65_ruhan-zone-2.ruha/tasks.hpp
@@ -1,7 +1,7 @@
 class CfgTasks {
     class RuhanZone2 {
-        title = CSTRING(Mission_Title);
-        description = CSTRING(Mission_Briefing);
+        title = LSTRING(Mission_Title);
+        description = LSTRING(Mission_Briefing);
         icon = "target";
 
         conditionEventsSuccess[] = { "BangingComplete" };
@@ -11,8 +11,8 @@ class CfgTasks {
 
     // Task for Bravo platoon
     class Survive {
-        title = CSTRING(Task_Survive_Title);
-        description = CSTRING(Task_Survive_Description);
+        title = LSTRING(Task_Survive_Title);
+        description = LSTRING(Task_Survive_Description);
         // createdShowNotification = "true";
         owners[] = { "zeus", "bravoHq", "bravo1", "bravo2" };
         marker = "sys_marker_second_line";
@@ -26,8 +26,8 @@ class CfgTasks {
 
     // Task for Alpha platoon
     class ReachTheConstructionSite : Survive {
-        title = CSTRING(Task_ReachTheConstructionSite_Title);
-        description = CSTRING(Task_ReachTheConstructionSite_Description);
+        title = LSTRING(Task_ReachTheConstructionSite_Title);
+        description = LSTRING(Task_ReachTheConstructionSite_Description);
         owners[] = { "zeus", "alphaHq", "alpha1", "alpha2", "alpha3", "alpha4" };
         icon = "run";
 
@@ -39,8 +39,8 @@ class CfgTasks {
 
     // Friendly Tracker Jammed
     class FriendlyTrackerJammed {
-        title = CSTRING(Task_FriendlyTrackerJammed_Title);
-        description = CSTRING(Task_FriendlyTrackerJammed_Description);
+        title = LSTRING(Task_FriendlyTrackerJammed_Title);
+        description = LSTRING(Task_FriendlyTrackerJammed_Description);
         parentTask = "RuhanZone2";
         icon = "radio";
 
@@ -49,8 +49,8 @@ class CfgTasks {
 
     // Counterattack in Ruha
     class Counterattack {
-        title = CSTRING(Task_Counterattack_Title);
-        description = CSTRING(Task_Counterattack_Description);
+        title = LSTRING(Task_Counterattack_Title);
+        description = LSTRING(Task_Counterattack_Description);
         createdShowNotification = "true";
         parentTask = "RuhanZone2";
         icon = "attack";
@@ -62,8 +62,8 @@ class CfgTasks {
 
     // Capture Officer
     class CaptureOfficer {
-        title = CSTRING(Task_CaptureOfficer_Title);
-        description = CSTRING(Task_CaptureOfficer_Description);
+        title = LSTRING(Task_CaptureOfficer_Title);
+        description = LSTRING(Task_CaptureOfficer_Description);
         createdShowNotification = "true";
         parentTask = "RuhanZone2";
         icon = "kill";
@@ -75,8 +75,8 @@ class CfgTasks {
 
     // Evac
     class Evac {
-        title = CSTRING(Task_Evac_Title);
-        description = CSTRING(Task_Evac_Description);
+        title = LSTRING(Task_Evac_Title);
+        description = LSTRING(Task_Evac_Description);
         createdShowNotification = "true";
         parentTask = "RuhanZone2";
         icon = "run";
