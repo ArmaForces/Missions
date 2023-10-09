044069ae1818da30e524f69aa31c1c7ba7429a88
3Mydlo3 <mydlo333@interia.pl>
2023-10-09 23:42:28 +0200

    Convert some tasks to CfgTasks

--- COOP/co_42_back-to-2018.panthera3/CfgTasks.hpp
+++ COOP/co_42_back-to-2018.panthera3/CfgTasks.hpp
@@ -1,4 +1,83 @@
 class CfgTasks
 {
+    tag = "AFP_Tasks";
+
     // Put your tasks here
+    class GreenPanther
+    {
+        title = CSTING(Mission_Title);
+        description = CSTRING(Mission_Description);
+        icon = "whiteboard";
+    };
+    
+    class ArmyDepot
+    {
+        parentTask = "GreenPanther";
+        icon = "attack";
+        marker = "sys_marker_army_depot";
+
+        conitionEventsSuccess[] = { "ArmyDepotSecured" };
+    };
+
+    class AirportIntel
+    {
+        parentTask = "GreenPanther";
+        icon = "documents";
+        marker = "sys_marker_airport";
+
+        conditionEventsSuccess[] = { "AirportIntelFound" };
+        conditionEventsFailed[] = { "AirportIntelDestroyed" };
+    };
+
+    class CaptureOfficer
+    {
+        parentTask = "GreenPanther";
+        icon = "target";
+        conditionEventsSuccess[] = { "OfficerCaptured" };
+    };
+
+    class CommsCenter
+    {
+        parentTask = "GreenPanther";
+        icon = "attack";
+        marker = "sys_marker_commsCenter";
+        
+        // TODO: Handle destruction
+        conditionEventsSuccess[] = { "CommsCenterCaptured", "CommsCenterDestroyed" };
+    };
+
+    /*
+        Cargo
+    */
+    class SecureCargo
+    {
+        parentTask = "GreenPanther";
+        icon = "container";
+
+        conditionEventsSuccess[] = { "AmmoRetrieved", "ContainerRetrieved" };
+        conditionEventsSuccessRequired = 2;
+    };
+
+    class TownTrucks
+    {
+        parentTask = "SecureCargo";
+        icon = "truck";
+        marker = "sys_marker_town_trucks";
+
+        conditionEventsSuccess[] = { "AmmoRetrieved" };
+    };
+
+    class IslandTrucks1 : TownTrucks
+    {
+        marker = "transport_island_empty";
+        conditionEventsSuccess[] = { "ContainerRetrieved" };
+    };
+
+    class IslandTrucks2 : TownTrucks
+    {
+        marker = "sys_marker_island_trucks_2";
+
+        conditionEventsCanceled[] = { "AirportIntelFound" };
+        conditionEventsSuccess[] = {};
+    };
 };
--- COOP/co_42_back-to-2018.panthera3/initServer.sqf
+++ COOP/co_42_back-to-2018.panthera3/initServer.sqf
@@ -3,3 +3,17 @@
 if (!isNil QUOTE(RESPAWN_HELPER_VR)) then {
     createMarker ["respawn", position RESPAWN_HELPER_VR];
 };
+
+["AirportIntelFound", {
+    "transport_town" setMarkerPos (getMarkerPos "transport_town_empty");
+    "transport_island" setMarkerPos (getMarkerPos "transport_island_empty");
+}] call CBA_fnc_addEventHandler;
+
+["AmmoRetrieved", {
+    deleteMarker "transport_town";
+    deleteMarker "transport_town_empty";
+}] call CBA_fnc_addEventHandler;
+
+["CommsCenterSecured", {
+    deleteMarker "marker_3";
+}] call CBA_fnc_addEventHandler;
